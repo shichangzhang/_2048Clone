2b409b3dd9644315818d9b9558ee6fb7b091972f
shichangzhang <shiz2@student.unimelb.edu.au>
2016-11-29 01:58:10 +1100

    Added random colours for tiles, still can't quite work out how to move and merge cells tho

--- newscript.js
+++ newscript.js
@@ -2,6 +2,9 @@ window.onload = function(){
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+//Colour of tiles
+var colours = new Array();
+
 //Board background
 var boardWidth = 290;
 var boardHeight = 290;
@@ -14,11 +17,12 @@ var tileHeight = 60;
 var tilePadding = 10;
 var tileOffsetTop = 185;
 var tileOffsetLeft = 25;
-var boardBackground = [];
+var board = [];
+
 for(c=0; c<tileColumnCount; c++) {
-    boardBackground[c] = [];
+    board[c] = [];
     for(r=0; r<tileRowCount; r++) {
-        boardBackground[c][r] = { x: 0, y: 0, number: 0, isNew: false};
+        board[c][r] = { x: 0, y: 0, number: 0, isNew: false};
     }
 }
 
@@ -34,48 +38,84 @@ function drawBoard() {
     for(c=0; c<tileColumnCount; c++) {
         for(r=0; r<tileRowCount; r++) {
             
-                var tileX = (c*(tileWidth+tilePadding))+tileOffsetLeft;
-                var tileY = (r*(tileHeight+tilePadding))+tileOffsetTop;
-                ctx.beginPath();
-                ctx.rect(tileX, tileY, tileWidth, tileHeight);
-                ctx.fillStyle = "#CFC1B8";
-                ctx.fill();
-                ctx.closePath();
-            
+            //draw background
+        	var tileX = (c*(tileWidth+tilePadding))+tileOffsetLeft;
+        	var tileY = (r*(tileHeight+tilePadding))+tileOffsetTop;
+        	ctx.beginPath();
+        	ctx.rect(tileX, tileY, tileWidth, tileHeight);
+        	ctx.fillStyle = "#CFC1B8";
+        	ctx.fill();
+        	ctx.closePath();
+
+        	if(board[c][r].number > 1) {
+        		//draw tile
+        		ctx.beginPath();
+        		ctx.rect(tileX, tileY, tileWidth, tileHeight);
+        		ctx.fillStyle = colours[Math.log2(board[c][r].number)-1];
+        		ctx.fill();
+        		ctx.closePath();
+        		//draw number
+        		ctx.font = "24px Impact, Charcoal, sans-serif";
+     			ctx.textAlign = "center";
+        		ctx.fillStyle = "white";
+        		ctx.fillText(board[c][r].number, tileX+tileWidth/2, tileY+tileHeight/2+9,60);
+        	}
         }
     }
 }
 
+generateColours();
+drawBoard();
+spawnRandomTile();
+drawBoard();
+spawnRandomTile();
 drawBoard();
 
 function draw() {
+	
+	
 
 }
 
-//setInterval(draw, 10);
+//setInterval(draw, 1000);
 
-function spawnRandomTile(){
+function move(x=-1, y=0) {
+	if(){
+		
+	}
+}
+
+function spawnRandomTile() {
 	var number = 2+2*Math.floor(2*Math.random());
-	var rand = Math.floor(16*Math.random());
 	var count = 0;
 	var blanks = new Array();
-	for(var i = 0; i < dim; i++){
-		for(var j = 0; j < dim; j++){
-			var tile = gameBoard[i][j];
-			if(tile.isBlank){
+
+	for(var i = 0; i < tileColumnCount; i++) {
+		for(var j = 0; j < tileRowCount; j++) {
+			var tile = board[i][j];
+			if(tile.number === 0){
 				blanks.splice(0,0,tile);
 			}
 		}
 	}
 
-	if(blanks.length != 0){
+	if(blanks.length != 0) {
+		var rand = Math.floor(blanks.length*Math.random());
 		var randTile = blanks[rand%(blanks.length)];
 		
-		randTile.isNew = true;
+		randTile.isNew = false;
 		randTile.number = number;
-		randTile.html.innerHTML = number;
-		randTile.html.className += " t"+number;
 	}
+}
+
+function generateColours() {
+	for(var i = 0; i < 20; i++) {
+		colours[i] = randomColour();
+	}
+}
+
+function randomColour() {
+	return "rgba("+Math.floor(256*Math.random())+", "+Math.floor(256*Math.random())+", "+Math.floor(256*Math.random())+", 1)";
 }
 
 };
